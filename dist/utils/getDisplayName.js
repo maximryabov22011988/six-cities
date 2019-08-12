a62ac46da6319772c222c000b14e42856ffbf8c0
Ryabov Maxim <designmr2015@gmail.com>
2019-08-13 01:13:08 +0400

    Fix e2e test

---
+++ dist/utils/getDisplayName.js
@@ -0,0 +1,7 @@
+'use strict';
+Object.defineProperty(exports, '__esModule', { value: true });
+function getDisplayName(WrappedComponent) {
+  return WrappedComponent.displayName || WrappedComponent.name || 'Component';
+}
+exports.default = getDisplayName;
+//# sourceMappingURL=getDisplayName.js.map
