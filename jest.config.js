0881e35795cd1cc472be68e9159556d476529a20
Ryabov Maxim <designmr2015@gmail.com>
2019-08-16 01:02:38 +0400

    Fix import module error

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,10 @@
 module.exports = {
+  roots: ['<rootDir>/src'], // fix import module error
   transform: {
-    '^.+\\.tsx?$': 'ts-jest',
-    '^.+\\.js?$': 'babel-jest',
+    '^.+\\.(ts|tsx)?$': 'ts-jest',
+    '^.+\\.(js|jsx)$': 'babel-jest',
   },
   testRegex: '.test.(js?|jsx?|tsx?)$',
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  testURL: 'http://localhost/',
 };
